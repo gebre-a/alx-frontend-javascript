cedbc85e2e79338ba92c0b504aa0d7dd4bd5f5f4
gebre-a <kidanalemayehu12@gmail.com>
2024-06-04 00:14:11 -0700

    0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,13 @@
-export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') {
-    return '';
-  }
+const cleanSet = (set, startString) => {
+  const strings = [];
 
-  const result = [];
-
-  set.forEach((item) => {
-    if (item.startsWith(startString)) {
-      result.push(item.slice(startString.length));
+  if (startString === '' || typeof startString !== 'string') return '';
+  set.forEach((s) => {
+    if (typeof s === 'string' && s.startsWith(startString)) {
+      strings.push(s.slice(startString.length));
     }
   });
+  return strings.join('-');
+};
 
-  return result.join('-');
-}
+export default cleanSet;
