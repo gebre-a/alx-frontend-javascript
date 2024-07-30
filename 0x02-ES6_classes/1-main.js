df8f9410fe839caebcc27323bda56ee794e9a6ef
gebre-a <kidanalemayehu12@gmail.com>
2024-07-30 06:20:27 -0700

    task 3

---
+++ 0x02-ES6_classes/1-main.js
@@ -0,0 +1,5 @@
+
+import initializeRooms from './1-make_classrooms';
+
+console.log(initializeRooms());
+
---
+++ 0x02-ES6_classes/3-main.js
@@ -0,0 +1,6 @@
+
+import Currency from "./3-currency";
+
+const dollar = new Currency('$', 'Dollars');
+console.log(dollar.displayFullCurrency());
+
